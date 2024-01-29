7f5aaabad2414a54590db0ba3db1546aa837b460
Daniel Fronczak <Daniel Fronczak>
2024-01-29 20:49:18 +0100

    Deployed website

--- src/Components/Statystyki.js
+++ src/Components/Statystyki.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { PieChart, Pie, Tooltip, Cell } from 'recharts'; // Import Cell component
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
 import './Statystyki.css';
 import Animacja2 from './Animacja2';
+
 function Statystyki() {
   const data = [
     { name: 'HTML', users: 5 },
@@ -13,40 +14,37 @@ function Statystyki() {
     { name: 'Source Filmmaker', users: 2 },
   ];
 
-  const colors = ['#ADD8E6', '#33FF57', '#5733FF', '#E63244', '#F75E25', '#FFFF00', '#1616FF']; // Add your desired colors here
+  const colors = ['#ADD8E6', '#33FF57', '#5733FF', '#E63244', '#F75E25', '#FFFF00', '#1616FF'];
 
   return (
     <div id="glowne" className='glowne'>
       <div style={{ textAlign: 'center' }}>
-      <Animacja2 />
-      <section className="hidden2">          
-
-        <h1>Wykres umiejetosci</h1>
+        <Animacja2 />
+        <section className="hidden2">
+          <h1>Wykres umiejętności</h1>
         </section>
 
         <div className='Wykres'>
-        <section className="hidden2">          
-
-          <PieChart width={1400} height={700}>
-            <Pie
-              dataKey='users'
-              isAnimationActive={true}
-              data={data}
-
-              outerRadius={280}
-              label
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-              ))}
-            </Pie>
-            <Tooltip />
-          </PieChart>
+          <section className="hidden2">
+            <PieChart width={1200} height={700}>
+              <Pie
+                dataKey='users'
+                isAnimationActive={true}
+                data={data}
+                outerRadius={280}
+                label={({ name }) => name} 
+                labelLine={false} 
+              >
+                {data.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
           </section>
         </div>
       </div>
     </div>
-    
   );
 }
 
--- src/Components/Stopka.js
+++ src/Components/Stopka.js
@@ -6,9 +6,9 @@ const Stopka = () => {
     
     <div className="stopka">
       <ul>
-        <li> <a href="www.google.pl"> <i class="gg-instagram"></i> </a></li>
-        <li> <a href="www.google.pl"> <i class="gg-google"></i></a></li>
-        <li> <a href="www.google.pl"> <i class="gg-facebook"></i> </a></li>
+        <li> <a href="https://www.instagram.com/"> <i class="gg-instagram"></i> </a></li>
+        <li> <a href="https://polishtango.github.io/Portfolio/"> <i class="gg-google"></i></a></li>
+        <li> <a href="https://www.facebook.com/"> <i class="gg-facebook"></i> </a></li>
       </ul>
     </div>
   );
